610e98e13fa0c333ceeb8c5ebfbf43d636e1e13f
shencanet <santi.dionis@gmail.com>
2025-08-16 19:17:46 +0200

    ADD: make controller

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,44 +3,36 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Product;
 
 class ProductController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
+//OBTENER TODOS LOS PRODUCTOS
     public function index()
     {
-        //
+        $products = Product::all();
+        return view('products.index', compact('products'));// Retorna la vista con todos los productos
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
+
     public function create()
     {
         //
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
+
     public function store(Request $request)
     {
         //
     }
 
-    /**
-     * Display the specified resource.
-     */
+
     public function show(string $id)
     {
         //
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
+
     public function edit(string $id)
     {
         //
