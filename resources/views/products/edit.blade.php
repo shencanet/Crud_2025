6daf6c940fc5b2df70e16c801c9fab5bbfbb448e
shencanet <santi.dionis@gmail.com>
2025-09-10 21:27:37 +0200

    ADD vistas

--- resources/views/products/edit.blade.php
+++ resources/views/products/edit.blade.php
@@ -1,1 +1,26 @@
-<h1>editar</h1>
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <title>Laravel</title>
+</head>
+<body>
+    <h1>Editar Producto</h1>
+    <form action="{{ route('products.update', $product-> id) }}" method="POST">
+        @csrf
+        @method('PUT')
+        <label for="name">Nombre:</label>
+        <input type="text" id="name" name="name" value="{{ $product->name }}" required>
+        <br><br>
+        <label for="price">Precio:</label>
+        <input type="number" id="price" name="price" step="0.01" value="{{ $product->price }}" required>
+        <br><br>
+        <label for="description">Descripción:</label>
+        <textarea id="description" name="description" required>{{ $product->description }}</textarea>
+        <br><br>
+        <button type="submit">Actualizar</button>
+
+    </form>
+
+
+</body>
+</html>
--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -7,6 +7,14 @@
     <h1>Nuevo Producto</h1>
 
     <a href="{{ route ('products.index') }}">atras</a>
+    <br>
+    <label>ID:</label>
+    <p>{{ $product->id}}</p>
+        <label>Precio:</label>
+    <p>{{ $product->price}}</p>
+        <label>Descripcion:</label>
+    <p>{{ $product->description}}</p>
+    
 
 
 
