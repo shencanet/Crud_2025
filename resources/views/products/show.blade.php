3f5f59541bbab672fccd55175096ca7439d13056
shencanet <santi.dionis@gmail.com>
2025-09-10 21:06:05 +0200

    ADD vistas

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -1,1 +1,14 @@
-<h1>Mostrar</h1>
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <title>Laravel</title>
+</head>
+<body>
+    <h1>Nuevo Producto</h1>
+
+    <a href="{{ route ('products.index') }}">atras</a>
+
+
+
+</body>
+</html>
